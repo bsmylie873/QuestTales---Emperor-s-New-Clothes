54fd6e454ca8a2874cf93c12f7114653e6bff292
Brandon James Smylie <bsmylie02@qub.ac.uk>
2020-04-06 00:28:34 +0100

    Modifications

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
-from flask import Flask,render_template, request,jsonify,Response
+from flask import Flask, render_template, request, jsonify, Response, session, flash
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/')
 def home():
-    return render_template('home.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('overview.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['psw'] == 'password' and request.form['uname'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('Wrong password!')
+    return home()
+
 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port = 6789, debug = True)
+    app.secret_key = 'the most secret of keys'
+
+    app.run(host='0.0.0.0', port=6789, debug=True)
