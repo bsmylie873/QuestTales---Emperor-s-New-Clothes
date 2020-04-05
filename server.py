e5f1c12f13e5d228fb5e8da9c428097d75a433ef
Brandon James Smylie <bsmylie02@qub.ac.uk>
2020-04-05 18:34:32 +0100

    Home page beginnings

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def home():
-    return '<h1> Hello World </h1><p>My name is Brandon</p>'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port = 6789, debug = True)
