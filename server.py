c87c04fceb2ba2b551c0201d63f1d41cd22e5b79
Brandon James Smylie <bsmylie02@qub.ac.uk>
2020-04-06 20:24:09 +0100

    Webpages

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, Response, session, f
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
@@ -17,6 +18,35 @@ def login():
     else:
         flash('Wrong password!')
     return home()
+
+
+@app.route('/overview')
+def overview():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('overview.html')
+
+
+@app.route('/contact')
+def contact():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('about.html')
+
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
 
 
 if __name__ == '__main__':
