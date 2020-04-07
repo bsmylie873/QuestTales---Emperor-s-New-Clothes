7c76eb96bca8c5eb259b7ee5911091901d103b53
Brandon James Smylie <bsmylie02@qub.ac.uk>
2020-04-07 16:08:42 +0100

    Refresh

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify, Response, session, flash
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
