542cc666a366743219dd231b83d809f12edf818e
Brandon James Smylie <bsmylie02@qub.ac.uk>
2020-04-07 23:40:31 +0100

    Credit comments

---
+++ model.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from server import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    events = db.relationship('Event', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Event {}>'.format(self.event)
+
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
